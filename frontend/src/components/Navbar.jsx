60d38438b9e394dbf637be0014422f22d2823cf2
Sergi Centeno <sergicentenopla@gmail.com>
2026-07-04 14:07:22 +0000

    Rename app to FlatRanker

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-brand">🏠 Rankflat</Link>
+      <Link to="/" className="navbar-brand">🏠 FlatRanker</Link>
       <Link to="/add" className="navbar-add">+ Add flat</Link>
     </nav>
   )
