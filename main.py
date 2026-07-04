1f55e04cf7b7aadb39212b4ab7c9f68448e7f630
Sergi Centeno <sergicentenopla@gmail.com>
2026-07-04 16:52:41 +0000

    Add Vercel URL to CORS allowed origins

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://flat-ranker.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
